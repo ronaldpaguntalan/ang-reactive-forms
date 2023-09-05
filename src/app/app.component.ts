df63a72d2a5ab25eefed0615f105ccd175e78ab1
Ronald Allan Ramos Paguntalan <ronallanpaguntalan@gmail.com>
2023-09-05 22:35:51 +0800

    Reactive Forms :
    Continuation of Validation
    Getting value of reactive forms
    Nested Form Groups
    
    Angular Full Course - Complete Zero to Hero Angular full Tutorial
    
    Link: https://www.youtube.com/watch?v=CGLdH5ORX-Y

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,18 +9,40 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 })
 export class AppComponent {
   form : any;
+  // emailRegex: string = "[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,}$"
+  contactRegex: string = "[789][0-9]{9}"
 
   constructor(){
     this.form = new FormGroup({
       fullName: new FormControl('',[
         Validators.required,
-        Validators.minLength(5),
-        Validators.maxLength(10)
+        Validators.minLength(5)
       ]),
-      email: new FormControl(),
-      address: new FormControl()
+      email: new FormControl('',[
+        Validators.required,
+        // Validators.pattern(this.emailRegex)
+        Validators.email
+      ]),
+      
+      contactDetails: new FormGroup({
+        address: new FormControl('',[Validators.required]),
+        shippingAddress: new FormControl('',[Validators.required]),
+        contactNumber: new FormControl('',[
+          Validators.required,
+          Validators.pattern(this.contactRegex)
+        ])
+      })
     }); 
   }
 
   get fullName() {return this.form.get('fullName')}
+  get email() {return this.form.get('email')}
+  get address() {return this.form.get('contactDetails.address')}
+  get shippingAddress() {return this.form.get('contactDetails.shippingAddress')}
+  get contactNumber() {return this.form.get('contactDetails.contactNumber')}
+
+  onSubmit(){
+    console.log(this.form.value);
+    
+  }
 }
