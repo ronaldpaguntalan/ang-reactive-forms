6390c3f5cbc9c79f44526124d41e8f8765cd338b
Ronald Allan Ramos Paguntalan <ronallanpaguntalan@gmail.com>
2023-09-06 22:29:24 +0800

    Reactive Forms: Form Array and Form Builder
    Angular Full Course - Complete Zero to Hero Angular full Tutorial
    
    Link: https://www.youtube.com/watch?v=CGLdH5ORX-Y

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormArray, FormBuilder } from '@angular/forms';
 
 
 @Component({
@@ -12,12 +12,41 @@ export class AppComponent {
   // emailRegex: string = "[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,}$"
   contactRegex: string = "[789][0-9]{9}"
 
-  constructor(){
+
+
+  constructor( fb: FormBuilder ){
+    // Using FormBuilder
+    // this.form = fb.group({
+    //   fullName: ['', [
+    //     Validators.required,
+    //     Validators.minLength(5)
+    //   ]],
+
+    //   email: ['', [
+    //     Validators.required,
+    //     // Validators.pattern(this.emailRegex)
+    //     Validators.email
+    //   ]],
+
+    //   contactDetails: fb.group({
+    //     address: ['', Validators.required],
+    //     shippingAddress: ['', Validators.required],
+    //     contact: ['', [
+    //       Validators.required,
+    //       Validators.pattern(this.contactRegex)
+    //     ]]
+    //   }),
+
+    //   skills: fb.array([])
+    // })
+
+  //  Longer way of creating a Nested Form Group, Form Control, and Form Array
     this.form = new FormGroup({
       fullName: new FormControl('',[
         Validators.required,
         Validators.minLength(5)
       ]),
+
       email: new FormControl('',[
         Validators.required,
         // Validators.pattern(this.emailRegex)
@@ -31,8 +60,11 @@ export class AppComponent {
           Validators.required,
           Validators.pattern(this.contactRegex)
         ])
-      })
+      }),
+
+      skills : new FormArray([])
     }); 
+  
   }
 
   get fullName() {return this.form.get('fullName')}
@@ -40,9 +72,23 @@ export class AppComponent {
   get address() {return this.form.get('contactDetails.address')}
   get shippingAddress() {return this.form.get('contactDetails.shippingAddress')}
   get contactNumber() {return this.form.get('contactDetails.contactNumber')}
+  get Skills(){return this.form.get('skills') as FormArray}
 
   onSubmit(){
     console.log(this.form.value);
     
+  }
+
+  addSkills(skills: HTMLInputElement){
+   this.Skills.push(
+    new FormControl(skills.value)
+   );
+   console.log(this.form.value);
+
+   skills.value = '';
+  }
+
+  removeSkills(index: number){
+    this.Skills.removeAt(index);
   }
 }
